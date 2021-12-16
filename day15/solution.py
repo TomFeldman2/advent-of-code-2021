da669b0461e5bcdbb58549483dfceb1d4bb91752
tom347 <tomfeldman2002@gmail.com>
2021-12-16 12:45:59 +0200

    improved day 15

--- day15/solution.py
+++ day15/solution.py
@@ -24,7 +24,8 @@ class ListEntry:
 def parse(string: str) -> Data:
     return [[int(x) for x in line] for line in string.splitlines()]
 
-def find_shortest_path(data: Data) -> int:
+
+def part1(data: Data) -> int:
     def neighbors(x: int, y: int) -> list[tuple[int, int]]:
         return list(filter(lambda pos: 0 <= pos[0] < len(data) and 0 <= pos[1] < len(data[0]), [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]))
 
@@ -43,44 +44,52 @@ def find_shortest_path(data: Data) -> int:
             if (i, j) not in close_set and len(list(filter(lambda n: (n.row, n.col) == (i, j), open_list))) == 0:
                 heapq.heappush(open_list, ListEntry(row=i, col=j, cost=node.cost + data[i][j]))
 
-def part1(data: Data) -> int:
-    return find_shortest_path(data)
 
 def clone_data(data: Data) -> Data:
     return list(map(lambda row: list(map(lambda x: 1 if x == 9 else x + 1, row)), data))
 
+
 def part2(data: Data) -> int:
-    new_data = [data]
+    num_rows, num_cols = len(data), len(data[0])
 
-    for _ in range(4):
-        new_data.append(clone_data(new_data[-1]))
+    def neighbors(x: int, y: int) -> list[tuple[int, int]]:
+        return list(filter(lambda pos: 0 <= pos[0] < 5 * num_rows and 0 <= pos[1] < 5 * num_cols, [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]))
 
-    joined = [[val for sublist in list_of_lists for val in sublist] for list_of_lists in list(zip(*new_data))]
-    
-    new_data = [joined]
-    for _ in range(4):
-        new_data.append(clone_data(new_data[-1]))
+    open_list = [ListEntry(row=0, col=0, cost=0)]
+    visited_set = {(0, 0)}
 
-    for row in new_data[1:]:
-        joined.extend(row)
-        
-    return find_shortest_path(joined)
+    while True:
+        node = heapq.heappop(open_list)
 
+        if node.row == 5 * num_rows - 1 and node.col == 5 * num_cols - 1:
+            return node.cost
+
+        for (i, j) in neighbors(node.row, node.col):
+            if (i, j) in visited_set:
+                continue
+
+            orig_cost = data[i % num_rows][j % num_cols]
+            orig_cost += (int(i // num_rows) + int(j // num_cols))
+            if orig_cost > 9:
+                orig_cost %= 9
+
+            heapq.heappush(open_list, ListEntry(row=i, col=j, cost=node.cost + orig_cost))
+            visited_set.add((i, j))
 
 
 if __name__ == '__main__':
     aoc = AoC(day=15, parse_fn=parse, part1_fn=part1, part2_fn=part2)
-#     example_data = parse("""1163751742
-# 1381373672
-# 2136511328
-# 3694931569
-# 7463417111
-# 1319128137
-# 1359912421
-# 3125421639
-# 1293138521
-# 2311944581""")
-#     aoc.solve(example_data)
+    #     example_data = parse("""1163751742
+    # 1381373672
+    # 2136511328
+    # 3694931569
+    # 7463417111
+    # 1319128137
+    # 1359912421
+    # 3125421639
+    # 1293138521
+    # 2311944581""")
+    #     aoc.solve(example_data)
 
     aoc.solve()
-    aoc.submit()
+    # aoc.submit()
